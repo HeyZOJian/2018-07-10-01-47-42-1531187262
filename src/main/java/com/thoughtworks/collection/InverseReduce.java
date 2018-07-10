2d4162b9846cb4cc502ac6c49e0a9fd22d67f23f
“zojian” <hellozojian@gmail.com>
2018-07-11 00:54:08 +0800

    feat: InverseReduce.java done with lambda and CollectionOperator

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,21 +1,37 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+//        List<Integer> resultList = new ArrayList<>();
+//        number -= 2;
+//        while (number > 0) {
+//            resultList.add(number);
+//            number -= 2;
+//        }
+//        return resultList;
+        if (number % 2 == 0) {
+            return new CollectionOperator().getEvenListByIntervals(number, 0);
+        } else {
+            return new CollectionOperator()
+                    .getListByInterval(number - 2, 1)
+                    .stream()
+                    .filter((x) -> x % 2 == 1)
+                    .collect(Collectors.toList());
+        }
+
     }
 }
