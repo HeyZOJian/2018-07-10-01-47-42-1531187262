e980560f286b06d2afa4056c64758e0b4b2f3412
“zojian” <hellozojian@gmail.com>
2018-07-11 00:45:01 +0800

    feat: Flaten.java done without lambda

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,35 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        for (Integer[] row:array){
+            for (Integer col:row){
+                resultList.add(col);
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        Set<Integer> resultSet = new LinkedHashSet<>();
+        for (Integer[] row:array){
+            for (Integer col:row){
+                resultSet.add(col);
+            }
+        }
+        return new ArrayList<>(resultSet);
     }
 }
